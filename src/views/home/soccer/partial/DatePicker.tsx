1c0ab5162a9b7cb4681f816e35f84c3b4393ef52
민혁 석 <seok971104@nexon.co.kr>
2024-08-02 20:20:31 +0900

    change date picker library and customize calendar css

--- src/views/home/soccer/partial/DatePicker.tsx
+++ src/views/home/soccer/partial/DatePicker.tsx
@@ -1,15 +1,29 @@
-import '../styles/DatePicker.scss'
+import '../styles/ReactCalendar.scss'
 
 import { observer } from 'mobx-react-lite'
 import React, { useState } from 'react'
-import { DayPicker } from 'react-day-picker'
-import 'react-day-picker/dist/style.css'
+import Calendar from 'react-calendar'
+import { AppStore } from '@stores/AppStore'
+import { useStore } from 'mobx-store-provider'
+
+type Value = Date | null
 
 export const DatePicker = observer(() => {
-  const [selected, setSelected] = useState<Date>()
+  const appStore = useStore(AppStore)
+  const [value, setValue] = useState<Value>(new Date())
+
+  const handleChange = (date: Date) => {
+    setValue(date)
+  }
+
   return (
-    <div className="w-full max-w-sm mx-auto">
-      <DayPicker mode="single" selected={selected} onSelect={setSelected} style={{ width: '200px', margin: '0 auto' }} />
+    <div className={`w-full max-w-sm mx-auto`}>
+      <Calendar
+        onChange={(date) => handleChange(date as Date)}
+        value={value}
+        locale="en-US"
+        className={`${appStore.selectedtheme === 'dark' ? 'dark-mode' : ''}`}
+      />
     </div>
   )
 })
